ed1c8649f58f9bc1ff90bfba5abd79a4f0517839
echoeszzz <ruzinivan1@gmail.com>
2022-12-12 13:39:40 +0300

    added task21

--- src/task21/MyArray.java
+++ src/task21/MyArray.java
@@ -1,23 +1,23 @@
 package task21;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class MyArray<ValueType> {
-    private ValueType[] array;
+    private List<ValueType> list;
 
-    public MyArray(ValueType[] array) {
-        this.array = array;
+    public MyArray(ValueType[] arr) {
+        list.addAll(Arrays.asList(arr));
     }
 
     public ValueType get(int index) {
-        return array[index];
+        return list.get(index);
     }
 
-    public void saveToList(List<ValueType> list) {
-        list.addAll(List.of(array));
-        for (int i = 0; i < 5; i++) {
-            System.out.println(array[i]);
+    public void printFirstFive() {
+        for (int i = 0; i < list.size() && i < 5; i++) {
+            System.out.println(list.get(i));
         }
     }
 }
